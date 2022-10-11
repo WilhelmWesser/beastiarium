7ca71274409bed99c003b940e4825869fe3fc578
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-10-11 22:58:36 +0300

    BSTRM. + Initial DB start

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {AppModule} from './app.module';
+import {Model} from 'objection'
+import knexConfig from "../knexfile";
+import {ENV} from "./common/enums/enums";
+import Knex from "knex";
+
+Model.knex(Knex(knexConfig[ENV.APP.NODE_ENV]))
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn']
+  });
+
+  await app.listen(ENV.APP.SERVER_PORT);
 }
 bootstrap();
--- backend/src/server.ts
+++
@@ -1,6 +0,0 @@
-import {Model} from 'objection'
-import knexConfig from "../knexfile";
-import {ENV} from "./common/enums/enums";
-import Knex from "knex";
-
-Model.knex(Knex(knexConfig[ENV.APP.NODE_ENV]))
