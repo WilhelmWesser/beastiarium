1bd4e30108246dace8058b6c71b49c950a337f5a
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-10-18 23:33:10 +0300

    BSTRM: moved from Knex and Objection to GraphQL and Prisma

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import {AppModule} from './modules/app.module';
-import {Model} from 'objection'
-import knexConfig from "../knexfile";
 import {ENV} from "./common/enums/enums";
-import Knex from "knex";
-
-Model.knex(Knex(knexConfig[ENV.APP.NODE_ENV]))
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
