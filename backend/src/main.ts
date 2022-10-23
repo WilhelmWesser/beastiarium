d9530fc35417bdcb35b19f26c1b90475ee251b6c
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-10-23 14:30:33 +0300

    BSTRM-2: + prisma initial migration and db schema fix in readme

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import {AppModule} from './modules/app.module';
 import {ENV} from "./common/enums/enums";
+import { PrismaService } from "./services/services";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn']
   });
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
 
   await app.listen(ENV.APP.SERVER_PORT);
 }
--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from '../controllers/app.controller';
 import { AppService } from '../services/app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLDefinitionsFactory, GraphQLModule } from "@nestjs/graphql";
+import { ENV } from '../common/enums/enums';
+import { join } from 'path';
+
+const definitionsFactory = new GraphQLDefinitionsFactory();
+definitionsFactory.generate({
+  typePaths: ['src/data/schemas/*.graphql'],
+  path: join(process.cwd(), 'src/data/schemas/ts-generated/graphql.ts'),
+  outputAs: 'class',
+  emitTypenameField: true,
+  watch: true,
+});
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: ENV.APP.NODE_ENV === 'development',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
