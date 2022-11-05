0b956f1eaa5be0ac569f734a4edc3dd99388ad76
wilhelm wesser <wilhelmwesser228@gmail.com>
2022-11-05 19:04:03 +0200

    Initial setup is complete

--- backend/src/modules/app.module.ts
+++ backend/src/modules/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ENV } from './common/enums/enums';
+import { ENV } from '../common/enums/enums';
 import { join } from 'path';
 import { PrismaService } from 'src/services/services';
 import { AppResolver } from 'src/resolvers/app.resolver';
@@ -11,7 +11,11 @@ import { AppResolver } from 'src/resolvers/app.resolver';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: ENV.APP.NODE_ENV === 'development',
-      autoSchemaFile: join(process.cwd(), 'src/schemas/schema.gql'),
+      typePaths: ['src/schemas/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/schemas/graphql.ts'),
+        outputAs: 'class',
+      },
       installSubscriptionHandlers: true,
     }),
   ],
