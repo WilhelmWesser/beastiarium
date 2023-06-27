56611f699d31940f6fe86a806a640b663f169820
wilhelm wesser <wilhelmwesser228@gmail.com>
2023-06-27 23:55:06 +0300

    BSTRM6: added bg image for beasts catalog page

---
+++ frontend/src/app/beasts/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import styles from "./styles.module.scss"
+
+type Props = {}
+const BeastPage: FC<Props> = () => {
+  return (
+    <div className={styles.beastsPageWrapper}>
+      <div className={styles.beastsPage}>
+
+      </div>
+    </div>
+  )
+}
+
+export default BeastPage
